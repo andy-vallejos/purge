f8f2341b2cd62bc5557ffdb1df2e9957fa620a60
Andy Gildo Vallejos Bascope <andyvallejosgb@gmail.com>
2026-06-04 00:53:34 -0400

    feat: add mockdata

---
+++ src/data/users.js
@@ -0,0 +1,17 @@
+export const users = [
+  {
+    nombres: "Andy Gildo",
+    apellidos: "Vallejos Bascope",
+    mail: "andyvallejosgb@gmail.com",
+    password: "admin123",
+    auth: false,
+  },
+
+  {
+    nombres: "Gaby",
+    apellidos: "Alcala",
+    mail: "gab@gmail.com",
+    password: "algo123",
+    auth: false,
+  },
+];
