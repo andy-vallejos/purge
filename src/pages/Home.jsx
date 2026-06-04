ffe90cbffea7c14ea40482300970f011667b3ee2
Andy Gildo Vallejos Bascope <andyvallejosgb@gmail.com>
2026-06-04 00:54:56 -0400

    feat: add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+export default function Home() {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    logout();
+
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <h1>Hola a la home</h1>
+      <button onClick={handleSubmit}>Desloguear</button>
+    </>
+  );
+}
