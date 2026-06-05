9dfd19f22b03938fe2fa81cd90b752c1dcb2f7d5
Andy Gildo Vallejos Bascope <andyvallejosgb@gmail.com>
2026-06-05 04:21:38 -0400

    refactor: add outlet

--- src/layouts/LayoutPrincipal/LayoutPrincipal.jsx
+++ src/layouts/LayoutPrincipal/LayoutPrincipal.jsx
@@ -1,15 +1,16 @@
 import style from "./LayoutPrincipal.module.css";
-import Router from "../../routes/Router";
 import { NavPrincipal } from "../../components";
+import { Outlet } from "react-router-dom";
 
 export function LayoutPrincipal() {
   return (
     <div className={style.container}>
       <div className={style.decoration}></div>
       <div className={`${style.decoration} ${style.blue}`}></div>
+
       <NavPrincipal />
-      <main>
-        <Router />
+      <main className={style.main}>
+        <Outlet />
       </main>
       <footer>@2026 PURGE</footer>
     </div>
